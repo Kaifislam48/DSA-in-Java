cb7c21784944f2c8a5ac4b6dc40205b56d1e40a5
Kaif Islam <117916990+Kaifislam48@users.noreply.github.com>
2025-03-23 22:52:29 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String args[]){
+        int a = 5;
+        int b = 3;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        a = 10;
+        System.out.println("a = "+a);
+        String name = "Tony";
+        System.out.println("name = "+name);
+    }
+}
